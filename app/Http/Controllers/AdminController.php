9c5c67d59f3c18cf7633d07a384ce21e3ed9e96a
costa197 <kevinguardel@gmail.com>
2025-04-06 20:56:27 -0500

    cambios

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,4 +10,25 @@ class AdminController extends Controller
     {
         return view('admin.dashboard');
     }
+
+    public funntion dashboard(Request $request)
+    {
+      $query = equipo::$query();
+
+      if ($request->has('aprendiz_id')){
+          $query->where('aprendiz_id',$request->input('aprendiz_id'));
+      }
+      
+
+      
+      if ($request->has('estado')){
+        $query->where('estado',$request->input('estado'));
+    }
+
+    $equipos = $query->get();
+    $aprendices =Aprendiz::all();
+
+    return vieew('admin.dashboar', compact('equipos', 'aprendices'));
+
+    }
 }
